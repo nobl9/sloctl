59017429d1c3a23f327dcaf0804f445bed782bae
Piotr Kazulak <piotr.kazulak@nobl9.com>
2024-11-13 12:18:31 +0100

    clean the filepath

--- internal/budgetadjustments/events/inputreader.go
+++ internal/budgetadjustments/events/inputreader.go
@@ -3,12 +3,13 @@ package events
 import (
 	"io"
 	"os"
+	"path/filepath"
 )
 
 func readFile(path string) (data []byte, err error) {
 	if path == "" || path == "-" {
 		return io.ReadAll(os.Stdin)
 	}
-        path = filepath.Clean(path)
+	path = filepath.Clean(path)
 	return os.ReadFile(path) // #nosec G304
 }
