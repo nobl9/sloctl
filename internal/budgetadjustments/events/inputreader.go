682fe71e56bb32cbb6f2347b7649779193fa736f
Piotr Kazulak <74537667+piotrkazulak@users.noreply.github.com>
2024-11-13 11:55:27 +0100

    Update internal/budgetadjustments/events/inputreader.go
    Co-authored-by: Mateusz Hawrus <48822818+nieomylnieja@users.noreply.github.com>

--- internal/budgetadjustments/events/inputreader.go
+++ internal/budgetadjustments/events/inputreader.go
@@ -9,5 +9,6 @@ func readFile(path string) (data []byte, err error) {
 	if path == "" || path == "-" {
 		return io.ReadAll(os.Stdin)
 	}
+        path = filepath.Clean(path)
 	return os.ReadFile(path) // #nosec G304
 }
