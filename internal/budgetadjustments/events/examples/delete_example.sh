eca1a9023680728da2a942557a656540fe1ae7f5
Piotr Kazulak <piotr.kazulak@nobl9.com>
2024-11-13 13:54:51 +0100

    rearange events update/delete examples

--- internal/budgetadjustments/events/examples/delete_example.sh
+++ internal/budgetadjustments/events/examples/delete_example.sh
@@ -1,15 +1,3 @@
-# Delete Adjustment Events using a file
-sloctl budgetadjustments events delete --adjustment-name=sample-adjustment-name -f /path/to/file/with/events/yaml
-
-# Delete Adjustment Events using stdin:
-echo '
-- eventStart: 2024-10-25T04:07:04Z
-  eventEnd: 2024-10-25T05:27:04Z
-  slos:
-  - project: test-project
-    name: sample-slo-10
-' | sloctl budgetadjustments events delete --adjustment-name=sample-adjustment-name -f -
-
 # Delete Adjustment Events using a file:
 cat <<EOF > ./events.yaml
 - eventStart: 2024-10-24T04:07:04Z
@@ -24,3 +12,12 @@ cat <<EOF > ./events.yaml
     name: sample-slo-2
 EOF
 sloctl budgetadjustments events delete --adjustment-name=sample-adjustment-name -f ./events.yaml
+
+# Delete Adjustment Events using stdin:
+echo '
+- eventStart: 2024-10-25T04:07:04Z
+  eventEnd: 2024-10-25T05:27:04Z
+  slos:
+  - project: test-project
+    name: sample-slo-10
+' | sloctl budgetadjustments events delete --adjustment-name=sample-adjustment-name -f -
--- internal/budgetadjustments/events/examples/update_example.sh
+++ internal/budgetadjustments/events/examples/update_example.sh
@@ -1,19 +1,3 @@
-# Update Adjustment Events using a file
-sloctl budgetadjustments events update --adjustment-name=sample-adjustment-name -f /path/to/file/with/events
-
-# Update Adjustment Events using stdin:
-echo '
-- eventStart: 2024-10-25T04:07:04Z
-  eventEnd: 2024-10-25T05:27:04Z
-  slos:
-  - project: test-project
-    name: sample-slo-10
-  update:
-    eventStart: 2024-10-25T03:07:04Z
-    eventEnd: 2024-10-25T04:27:04Z
-' | sloctl budgetadjustments events update --adjustment-name=sample-adjustment-name -f -
-
-
 # Update Adjustment Events using a file:
 cat <<EOF > ./events.yaml
 - eventStart: 2024-10-24T04:07:04Z
@@ -34,3 +18,15 @@ cat <<EOF > ./events.yaml
     eventEnd: 2024-10-25T04:27:04Z
 EOF
 sloctl budgetadjustments events update --adjustment-name=sample-adjustment-name -f ./events.yaml
+
+# Update Adjustment Events using stdin:
+echo '
+- eventStart: 2024-10-25T04:07:04Z
+  eventEnd: 2024-10-25T05:27:04Z
+  slos:
+  - project: test-project
+    name: sample-slo-10
+  update:
+    eventStart: 2024-10-25T03:07:04Z
+    eventEnd: 2024-10-25T04:27:04Z
+' | sloctl budgetadjustments events update --adjustment-name=sample-adjustment-name -f -
