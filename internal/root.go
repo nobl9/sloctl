2d0d71bc359af91be02d6253901b391563f4e934
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-01-08 16:28:13 +0100

    post merge fixes

--- internal/root.go
+++ internal/root.go
@@ -18,7 +18,7 @@ const programName = "sloctl"
 
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
-func Execute() {
+func Execute() error {
 	return NewRootCmd().Execute()
 }
 
