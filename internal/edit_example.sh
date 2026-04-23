ca2f61fd0c8417d884c150dd3e22d039c3573f0e
Mateusz Hawrus <mateusz.hawrus@nobl9.com>
2026-04-23 16:32:42 +0200

    docs: show alternative editor usage

--- internal/edit_example.sh
+++ internal/edit_example.sh
@@ -1,6 +1,9 @@
 # Edit one SLO from your default Project.
 sloctl edit slo my-slo
 
+# Use an alternative editor.
+SLOCTL_EDITOR="nano" sloctl edit project default
+
 # Edit one Service from a specific Project.
 sloctl edit service my-service -p my-project
 
